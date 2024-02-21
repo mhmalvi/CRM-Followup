b96733798200d50feb1e9b7eaf2fabbdc7dbc717
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-02-21 11:22:07 +0600

    port changed to 3002

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
-server.listen(7000, () => {
+server.listen(3002, () => {
   console.log("listening");
 });
 
