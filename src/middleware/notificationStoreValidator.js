90e978c9ad0f969bf38a4d687e3b34fa52741feb
TanjibQuad <tanjib@quadque.tech>
2024-02-29 15:15:16 +0600

    env update

--- src/middleware/notificationStoreValidator.js
+++ src/middleware/notificationStoreValidator.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
 
-const validateNotificationStoreData = [
-  body("title").isEmpty().withMessage("Title is empty"),
-  // Add more validation rules for other fields...
-];
+// const validateNotificationStoreData = [
+//   body("title").isEmpty().withMessage("Title is empty"),
+//   // Add more validation rules for other fields...
+// ];
 
 modules.export = { validateNotificationStoreData };
